c5d8e0a663aa0fe5f7cbc7636d6d1d29f59be430
v2parul <v2parul@gmail.com>
2020-08-13 19:02:43 +0200

    modified main.py in pythonproject2

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main(msg):
 def secondary(msg):
     print(msg)
     # two functions to demonstrate changes to main.py
-
+    #updating another funtion in another directory to create conflict.
+def tertiary(msg):
+    print(msg)
 
 main("hello world")
 secondary("another update")
+tertiary("hello"
